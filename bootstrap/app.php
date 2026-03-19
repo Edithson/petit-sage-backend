5e3851505133fe9c4a38fd15be2c94b86a2c075f
Edithson <moafogaus@gmail.com>
2026-03-19 02:36:55 +0100

    Feature : implémentation de Sentry pour le monitoring

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,6 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful;
+use Sentry\Laravel\Integration;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -18,5 +19,6 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
-        //
+        // Demande à Sentry de gérer les exceptions
+        Integration::handles($exceptions);
     })->create();
